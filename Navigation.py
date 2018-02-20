54b2fcc86159535c92154399d4d3f830a06c5231
RAED <rrrr53@hotmail.com>
2018-02-20 15:33:04 +0300

    Update Navigation.py

--- Navigation.py
+++ Navigation.py
@@ -1,17 +1,17 @@
-from enigma import eServiceCenter, eServiceReference, pNavigation, getBestPlayableServiceReference, iPlayableService, setPreferredTuner, eStreamServer
+from enigma import eServiceCenter, eServiceReference, eTimer, pNavigation, getBestPlayableServiceReference, iPlayableService, eActionMap, setPreferredTuner, eStreamServer
 from Components.ParentalControl import parentalControl
 from Components.SystemInfo import SystemInfo
 from Components.config import config, configfile
 from Tools.BoundFunction import boundFunction
 from Tools.StbHardware import getFPWasTimerWakeup
 from Tools import Notifications
-from time import time
+from time import time, localtime
 import RecordTimer
 import Screens.Standby
 import NavigationInstance
 import ServiceReference
 from Screens.InfoBar import InfoBar
-from Components.Sources.StreamService import StreamServiceList
+from sys import maxint
 
 # TODO: remove pNavgation, eNavigation and rewrite this stuff in python.
 class Navigation:
@@ -35,7 +35,6 @@ class Navigation:
 		self.currentlyPlayingService = None
 		self.RecordTimer = RecordTimer.RecordTimer()
 		self.__wasTimerWakeup = getFPWasTimerWakeup()
-		self.__isRestartUI = config.misc.RestartUI.value
 		startup_to_standby = config.usage.startup_to_standby.value
 		wakeup_time_type = config.misc.prev_wakeup_time_type.value
 		if self.__wasTimerWakeup:
@@ -54,9 +53,6 @@ class Navigation:
 
 	def wasTimerWakeup(self):
 		return self.__wasTimerWakeup
-
-	def isRestartUI(self):
-		return self.__isRestartUI
 
 	def dispatchEvent(self, i):
 		for x in self.event:
@@ -124,18 +120,18 @@ class Navigation:
 									setPreferredTuner(int(config.usage.frontend_priority_dvbt.value))
 									setPriorityFrontend = True
 							if SystemInfo["ATSC_priority_tuner_available"] and config.usage.frontend_priority_atsc.value != "-2":
-								if config.usage.frontend_priority_atsc.value != config.usage.frontend_priority.value:
-									setPreferredTuner(int(config.usage.frontend_priority_atsc.value))
-									setPriorityFrontend = True
+ 								if config.usage.frontend_priority_atsc.value != config.usage.frontend_priority.value:
+ 									setPreferredTuner(int(config.usage.frontend_priority_atsc.value))
+ 									setPriorityFrontend = True
 						elif type_service == 0xFFFF:
 							if SystemInfo["DVB-C_priority_tuner_available"] and config.usage.frontend_priority_dvbc.value != "-2":
 								if config.usage.frontend_priority_dvbc.value != config.usage.frontend_priority.value:
 									setPreferredTuner(int(config.usage.frontend_priority_dvbc.value))
 									setPriorityFrontend = True
 							if SystemInfo["ATSC_priority_tuner_available"] and config.usage.frontend_priority_atsc.value != "-2":
-								if config.usage.frontend_priority_atsc.value != config.usage.frontend_priority.value:
-									setPreferredTuner(int(config.usage.frontend_priority_atsc.value))
-									setPriorityFrontend = True
+ 								if config.usage.frontend_priority_atsc.value != config.usage.frontend_priority.value:
+ 									setPreferredTuner(int(config.usage.frontend_priority_atsc.value))
+ 									setPriorityFrontend = True
 						else:
 							if SystemInfo["DVB-S_priority_tuner_available"] and config.usage.frontend_priority_dvbs.value != "-2":
 								if config.usage.frontend_priority_dvbs.value != config.usage.frontend_priority.value:
@@ -176,12 +172,7 @@ class Navigation:
 		return ret
 
 	def getRecordings(self, simulate=False):
-		recs = self.pnav and self.pnav.getRecordings(simulate)
-		if not simulate and StreamServiceList:
-			for rec in recs[:]:
-				if rec.__deref__() in StreamServiceList:
-					recs.remove(rec)
-		return recs
+		return self.pnav and self.pnav.getRecordings(simulate)
 
 	def getCurrentService(self):
 		if not self.currentlyPlayingService:
